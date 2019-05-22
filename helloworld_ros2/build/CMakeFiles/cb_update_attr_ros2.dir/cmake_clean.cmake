4b319ed37daa4477bf72bf2973fb4a6091df26b4
ros2 <pantphotiou@gmail.com>
2019-05-22 20:45:20 +0300

    first commit

---
+++ helloworld_ros2/build/CMakeFiles/cb_update_attr_ros2.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cb_update_attr_ros2.dir/cb_update_attr.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/Easy.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/Exception.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/Form.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/Info.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/Multi.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/OptionBase.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/Options.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/cURLpp.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/internal/CurlHandle.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/internal/OptionList.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/internal/OptionSetter.cpp.o"
+  "CMakeFiles/cb_update_attr_ros2.dir/home/ros2/firos2/thirdparty/curlpp/src/curlpp/internal/SList.cpp.o"
+  "cb_update_attr.pdb"
+  "cb_update_attr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cb_update_attr_ros2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ helloworld_ros2/build/CMakeFiles/userlib_ros2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/userlib_ros2.dir/String_.cxx.o"
+  "CMakeFiles/userlib_ros2.dir/String_PubSubTypes.cxx.o"
+  "CMakeFiles/userlib_ros2.dir/userlib.cpp.o"
+  "CMakeFiles/userlib_ros2.dir/home/ros2/firos2/src/NGSIv2/idl/JsonNGSIv2.cxx.o"
+  "CMakeFiles/userlib_ros2.dir/home/ros2/firos2/src/NGSIv2/idl/JsonNGSIv2PubSubTypes.cxx.o"
+  "CMakeFiles/userlib_ros2.dir/home/ros2/firos2/src/jsoncpp.cpp.o"
+  "libuserlib.pdb"
+  "libuserlib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/userlib_ros2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
